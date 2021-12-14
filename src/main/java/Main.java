97d249dd749beef1986453338ab81a21ba6c2425
JesperJohansson95 <92728289+JesperJohansson95@users.noreply.github.com>
2021-12-14 13:35:27 +0100

    Skapat labels/textfields

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.awt.*;
 
 public class Main extends Application {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
@@ -17,9 +19,34 @@ public class Main extends Application {
 
         primaryStage.setTitle("Uthyrning");
         BorderPane borderPane = new BorderPane();
+
+
+
+        //Labels
+        Label lSearch = new Label("Sök");
+        Label lActor = new Label("Skådespelare");
+        Label lReleaseDate = new Label("Utgivningsdatum");
+        Label lCustomerName = new Label("Kundnamn");
+        Label lCustomerId = new Label("KundId");
+        Label lCustomerEmail = new Label("Kundmail");
+        Label lCustomerCity = new Label("Kundstad");
+
+
+        //Textfields
+        TextField tfSearch = new TextField();
+        TextField tfActor = new TextField();
+        TextField tfReleaseDate = new TextField();
+        TextField tfCustomerName = new TextField();
+        TextField tfCustomerId = new TextField();
+        TextField tfCustomerEmail = new TextField();
+        TextField tfCustomerCity = new TextField();
+
+
         Scene scene = new Scene(borderPane,1200,900);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
 
     }
 }
