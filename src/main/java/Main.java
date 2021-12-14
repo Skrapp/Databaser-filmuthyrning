fab358995f73956c6f886cdc34cb8719f62a397e
Sara Nilsson <nilssonsara97@gmail.com>
2021-12-14 14:15:50 +0100

    La till boxar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.awt.*;
 
 public class Main extends Application {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
@@ -20,7 +23,17 @@ public class Main extends Application {
         primaryStage.setTitle("Uthyrning");
         BorderPane borderPane = new BorderPane();
 
+        //Boxes
+        VBox vBoxLeft = new VBox();
+        VBox vBoxRight = new VBox();
 
+        HBox hBoxTop= new HBox();
+        /*VBox vBoxSearch = new VBox();
+        VBox vBoxSearch = new VBox();
+        VBox vBoxSearch = new VBox();
+        VBox vBoxSearch = new VBox();
+        VBox vBoxSearch = new VBox();
+        VBox vBoxSearch = new VBox();*/
 
         //Labels
         Label lSearch = new Label("Sök");
@@ -41,6 +54,20 @@ public class Main extends Application {
         TextField tfCustomerEmail = new TextField();
         TextField tfCustomerCity = new TextField();
 
+
+        //Add to Hbox
+        vBoxLeft.getChildren().addAll(lSearch, tfSearch, lActor, tfActor, lReleaseDate, tfReleaseDate);
+        vBoxRight.getChildren().addAll(lCustomerName, tfCustomerName, lCustomerCity, tfCustomerCity, lCustomerId, tfCustomerId, lCustomerEmail, tfCustomerEmail);
+
+        //Add to VBox
+        //hBoxTop.getChildren().addAll(vBoxLeft, vBoxRight);
+
+        vBoxRight.setAlignment(Pos.TOP_RIGHT);
+        vBoxLeft.setAlignment(Pos.TOP_LEFT);
+
+        //Add to borderPane
+        borderPane.setLeft(vBoxLeft);
+        borderPane.setRight(vBoxRight);
 
         Scene scene = new Scene(borderPane,1200,900);
         primaryStage.setScene(scene);
