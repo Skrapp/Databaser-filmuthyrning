075b73a10b293f4d4cdd38e3a35560aaba1d565b
Daniel S <90305939+SafstromDaniel@users.noreply.github.com>
2021-12-23 10:09:42 +0100

    Erik löste problemet med geometry. Ändrat i persistence filen och bytt location från point till geometry

--- src/main/java/AddToDatabase.java
+++ src/main/java/AddToDatabase.java
@@ -40,24 +40,24 @@ public class AddToDatabase {
 
             Point point = new Point(2, 2);
 
-            address.setAddress("Skogen");
-            address.setAddress2("Skogsvägen");
-            address.setDistrict("Knutby");
-            address.setCity_id(12);
-            address.setPostal_code("76437");
-            address.setPhone("122303032");
+            address.setAddress("Hos Jesper");
+            address.setAddress2("");
+            address.setDistrict("Salabacke");
+            address.setCity_id(16);
+            address.setPostal_code("75597");
+            address.setPhone("6371");
             address.setLocation(geometry);
             address.setLast_update(instant);
 
             entityManager.persist(address);
 
-           /* Query queryAdressID = entityManager.createNativeQuery("SELECT address_id FROM address WHERE address = Skogen AND district = Knutby AND city_id = 12 AND phone =122303032");
-            List<Integer> chosenArtist = queryAdressID.getResultList();
-            int addressID = chosenArtist.get(0);*/
+            /*Query queryAdressID = entityManager.createNativeQuery("SELECT address_id FROM address WHERE address = Skogen AND district = Knutby AND city_id = 12 AND phone =122303032");
+            List<Short> chosenID = queryAdressID.getResultList();
+            short addressID = chosenID.get(0);
 
-            /*customer.setFirst_name("Daniel3");
+            customer.setFirst_name("Daniel3");
             customer.setLast_name("Säfström3");
-            customer.setAddress_id(22); //adressID
+            customer.setAddress_id(addressID); //adressID
             customer.setStore_id(1);
             customer.setActive(1);
             customer.setEmail("Daniel@hot");
